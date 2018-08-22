ba37aec76dbe536e624a23c908f66ebf21365543
Wilian Pantoja <wilian.pantoja@sunsetswh.com>
2018-08-22 08:20:29 -0500

    servicios monedero y front finalizados

--- server.js
+++ server.js
@@ -21,27 +21,6 @@ const pool = new Pool({
 
 pool.connect();
 
-pool.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  //pool.end();
-});
-
-
-app.get('/db', async (req, res) => {
-  try {
-    const client = await pool.connect()
-    const result = await client.query('SELECT * FROM test_table');
-    res.json(result);
-    client.release();
-  } catch (err) {
-    console.error(err);
-    res.send("Error " + err);
-  }
-});
-
 app.get('/total', async (req, res) => {
   try {
     const client = await pool.connect()
@@ -54,57 +33,32 @@ app.get('/total', async (req, res) => {
   }
 });
 
-app.post('/actualizar', async (req, res) => {
+app.get('/monedero', async (req, res) => {
   try {
-    console.log(req.body);
-    var parametros = req.body;
     const client = await pool.connect()
-    const result = await client.query(`UPDATE public.monedero SET cantidad=${parametros.cantidad} WHERE denominacion='${parametros.denominacion}' AND tipo='${parametros.tipo}'`);
-    res.json(result);
-    client.release();
+    const result = await client.query('select * from monedero order by denominacion, tipo')
+    res.json(result)
+    client.release()
   } catch (err) {
-    console.error(err);
-    res.send("Error " + err);
+    console.error(err)
+    res.send("Error " + err)
   }
 });
 
-
-
+app.post('/actualizar', async (req, res) => {
+  try {
+    console.log(req.body)
+    var parametros = req.body
+    const client = await pool.connect()
+    const result = await client.query(`UPDATE public.monedero SET cantidad=${parametros.cantidad} WHERE denominacion='${parametros.denominacion}' AND tipo='${parametros.tipo}'`)
+    res.json(result)
+    client.release()
+  } catch (err) {
+    console.error(err)
+    res.send("Error " + err)
+  }
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port);
-console.log('server started '+ port);
-
-
-
-/**
- * Billetera virtual: donde se debe administrar y  visualizar:
-
- 
-
--Número de billetes por denominación
-
--El total de saldo que hay
-
-
-
-
-Host
-    ec2-107-22-221-60.compute-1.amazonaws.com
-Database
-    d8po5lj0ivhoid
-User
-    tflmxuvipzpzom
-Port
-    5432
-Password
-    8711f15fff25e0d56b910ff191bb4ef25f76d000069045957d17200c783fcaf5
-URI
-    postgres://tflmxuvipzpzom:8711f15fff25e0d56b910ff191bb4ef25f76d000069045957d17200c783fcaf5@ec2-107-22-221-60.compute-1.amazonaws.com:5432/d8po5lj0ivhoid
-Heroku CLI
-    heroku pg:psql postgresql-cubed-41102 --app monedero-fullstack
-
-
-
-
- */
+console.log('server started ' + port);
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,94 +1,85 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
+    <h1>Total: {{ total }}</h1>
+    <h2>Monedero</h2>
     <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
+      <li v-bind:key="index" v-for="(item, index) in monedero">
+        <div v-if="item.tipo=='B'" class="item-monedero">
+          <img class="billete" v-bind:src="item.imagen">
+          <input v-model="item.cantidad" v-on:change="actualizar(item)">
+        </div>
       </li>
     </ul>
-    <h2>Ecosystem</h2>
     <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
+      <li v-bind:key="index" v-for="(item, index) in monedero">
+        <div v-if="item.tipo=='M'" class="item-monedero">
+          <img class="billete" v-bind:src="item.imagen">
+          <input v-model="item.cantidad" v-on:change="actualizar(item)">
+        </div>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
+let url = 'http://localhost:5000'
+
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      total: 'Cargando total...',
+      monedero: []
+    }
+  },
+  mounted () {
+    axios({ method: 'GET', url: `${url}/total` }).then(result => {
+      console.log(result)
+      this.total = result.data.rows[0].total
+    }, error => {
+      console.error(error)
+    })
+    axios({ method: 'GET', url: `${url}/monedero` }).then(result => {
+      console.log(result)
+      this.monedero = result.data.rows
+      this.monedero.forEach(m => {
+        m.imagen = `${url}/static/img/img-${m.denominacion}-${m.tipo}.jpg`
+      })
+      this.monedero.sort((a, b) => {
+        return Number.parseInt(a.denominacion) - Number.parseInt(b.denominacion)
+      })
+    }, error => {
+      console.error(error)
+    })
+  },
+  methods: {
+    actualizar (item) {
+      console.log(item)
+      let parse = 0
+      try {
+        parse = Number.parseInt(item.cantidad)
+        if (isNaN(parse) || parse < 0) {
+          parse = 0
+        }
+      } catch (err) {
+        console.log(err)
+        parse = 0
+      }
+      item.cantidad = parse
+      axios({ method: 'POST', url: `${url}/actualizar`, data: {denominacion: item.denominacion, tipo: item.tipo, cantidad: item.cantidad}, 'headers': { 'content-type': 'application/json' } }).then(result => {
+        console.log(result.data)
+        // this.response = result.data
+        axios({ method: 'GET', url: `${url}/total` }).then(result => {
+          console.log(result)
+          this.total = result.data.rows[0].total
+        }, error => {
+          console.error(error)
+        })
+      }, error => {
+        console.error(error)
+      })
     }
   }
 }
@@ -102,12 +93,27 @@ h1, h2 {
 ul {
   list-style-type: none;
   padding: 0;
+  display: inline-table;
 }
 li {
-  display: inline-block;
+  display: block;
   margin: 0 10px;
+  margin-bottom: 10px;
+}
+.billete {
+  height: 80px;
+}
+.item-monedero {
+  vertical-align: middle;
+    align-items: center;
+    align-content: center;
+    display: flex;
 }
 a {
   color: #42b983;
+}
+input {
+  width: 40px;
+  text-align: center;
 }
 </style>
